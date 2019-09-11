18f93de753d3011ae961f18a847929d31abda6be
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2019-09-11 14:32:04 +0200

    Add new examples with sublevels for local_tasks module

--- local_tasks/src/Controller/LocalTasksController.php
+++ local_tasks/src/Controller/LocalTasksController.php
@@ -3,6 +3,7 @@
 namespace Drupal\local_tasks\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
 
 class LocalTasksController extends ControllerBase {
 
@@ -11,6 +12,33 @@ class LocalTasksController extends ControllerBase {
       '#markup' => $this->t('Node %title extra settings page', [
         '%title' => $node->getTitle(),
       ]),
+    ];
+  }
+
+  public function eshopSettings() {
+    return [
+      '#markup' => $this->t('This is a parent settings page. This page has two child pages: <a href="@products_settings_url">Products settings</a> and <a href="@categories_settings_url">Categories settings</a>', [
+        '@products_settings_url' => Url::fromRoute('local_tasks.products_settings')->toString(),
+        '@categories_settings_url' => Url::fromRoute('local_tasks.categories_settings')->toString(),
+      ]),
+    ];
+  }
+
+  public function eshopMetaSettings() {
+    return [
+      '#markup' => $this->t('This is a meta settings page'),
+    ];
+  }
+
+  public function productsSettings() {
+    return [
+      '#markup' => $this->t('This is a products settings page'),
+    ];
+  }
+
+  public function categoriesSettings() {
+    return [
+      '#markup' => $this->t('This is a categories settings page'),
     ];
   }
 
